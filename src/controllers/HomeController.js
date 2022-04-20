178ecd2be9f3509b5ed1f186691112755fc02021
neto schmitt <agneloschmittneto@gmail.com>
2022-04-19 22:08:10 -0300

    configurando e testando o Sequelize

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Agnelo',
+      sobrenome: 'Neto',
+      email: 'agneloneto@gmail.com',
+      idade: 39,
+      peso: 98,
+      altura: 1.87,
     });
+    res.json(novoAluno);
   }
 }
 
