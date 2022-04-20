2dab7fd84731c306575dcf9a01e391d8df5e4de2
neto schmitt <agneloschmittneto@gmail.com>
2022-04-20 15:04:28 -0300

    CRUD de usuarios

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,19 +4,17 @@ import userController from '../controllers/UserController';
 const router = new Router();
 
 router.post('/', userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 
 export default router;
 
 /* regras
-
 index -> lista todos usuarios -> GET
-
 store/create -> cria outro usuario -> POST
-
 delete -> apaga um usuarios -> DELETE
-
 show -> mostra um usuario -> GET
-
 update -> atualiza um usuario -> PACTH(altera somente um valor) ou PUT(altera obj inteiro)
-
 */
