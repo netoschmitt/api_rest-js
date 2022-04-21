40e6a09094e78e6ed4fb846c4eee477a8e67ab4a
neto schmitt <agneloschmittneto@gmail.com>
2022-04-21 17:38:45 -0300

    criando seeds para testar adicionar dados

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,7 +6,7 @@ import loginRequired from '../middlewares/loginRequired';
 const router = new Router();
 
 // Não deveria existir
-// router.get('/', userController.index); // Lista usuarios
+router.get('/', userController.index); // Lista usuarios
 // router.get('/:id', userController.show); // Lista usuario
 
 // ok
