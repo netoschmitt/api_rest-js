f1444c1317f68869609646cfbfe35fcb98b8bc75
neto schmitt <agneloschmittneto@gmail.com>
2022-04-20 21:23:23 -0300

    gerando o JWT do user

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import './src/database';
 import express from 'express';
 import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
+import tokenRoutes from './src/routes/tokenRoutes';
 
 class App {
   constructor() {
@@ -22,7 +23,8 @@ class App {
 
   routes() {
     this.app.use('/', homeRoutes);
-    this.app.use('/users', userRoutes);
+    this.app.use('/users/', userRoutes);
+    this.app.use('/tokens/', tokenRoutes);
   }
 }
 
