d0e05722b3c2de116eea34853b85403c99db500b
neto schmitt <agneloschmittneto@gmail.com>
2022-04-21 22:55:39 -0300

    Rotas de Alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Agnelo',
-      sobrenome: 'Neto',
-      email: 'agneloneto@gmail.com',
-      idade: 39,
-      peso: 98,
-      altura: 1.87,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
